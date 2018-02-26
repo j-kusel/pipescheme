a52f4f60ba77b6baf07a115b5904f0f07bc7de2c
Jordan Kusel <jordankusel@my.unt.edu>
2018-02-26 00:13:56 -0800

    changed maintenance splash page to absolute path

--- maintenance/express.js
+++ maintenance/express.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 module.exports = function() {
     const app = express();
     app.get('/', (req, res) => {
-        res.sendFile('./views/splash.html');
+        res.sendFile(path.join(__dirname + '/views/splash.html'));
     });
 
     return app;
