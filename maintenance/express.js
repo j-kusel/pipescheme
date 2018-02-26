4910d1d8da515cbbf581692a2daa62871ad028e1
Jordan Kusel <jordankusel@my.unt.edu>
2018-02-26 00:01:54 -0800

    added view engine declaration to maintenance express instance

--- maintenance/express.js
+++ maintenance/express.js
@@ -3,6 +3,7 @@ const express = require('express');
 module.exports = function() {
     const app = express();
     app.set('views', './views');
+    app.set('view engine', 'html');
     app.get('/', (req, res) => {
         res.render('splash');
     });
