d9a423c868cfc4d4518c03581d179bf4043d443f
Jordan Kusel <jordankusel@my.unt.edu>
2018-03-16 18:10:26 -0700

    troubleshooting https mixed loading issue

--- public/js/directives/psLeaflet.client.directive.js
+++ public/js/directives/psLeaflet.client.directive.js
@@ -21,8 +21,8 @@ angular.module('pipeScheme')
             });
 
             L.control.zoom({position: 'bottomleft'}).addTo(scope.map);
-            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(scope.map)
-            L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
+            L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(scope.map)
+            L.tileLayer('https://tile.stamen.com/toner/{z}/{x}/{y}.png', {
                 attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
             }).addTo(scope.map);
 
